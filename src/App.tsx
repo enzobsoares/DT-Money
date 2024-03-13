5fe0ba2f0b2803264a76a4dc3c2aea5010838177
enzobsoares <enzobragasoares@gmail.com>
2024-03-13 15:19:21 -0300

    initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
 
   return (
-    <h1>Salve</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Salve</h1>
+    </ThemeProvider>
   )
 }
 
